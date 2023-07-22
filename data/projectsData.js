b9c36633051e66b3b8cbff939912a5ecf23c6899
Abner Oliveira <abner.oliveira.ce@gmail.com>
2023-07-22 14:37:19 -0300

    feat: added template gov ce in projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,12 +6,10 @@ const projectsData = [
     href: 'https://convertt.vercel.app',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Template Governo do Ceará',
+    description: `Template ReactJs desenvolvido a partir do Manual de Identidade Visual do Governo do Estado do Ceará - Brasil.`,
+    imgSrc: '/static/images/id-gov-ce-2023.jpg',
+    href: 'http://id-gov-ce-2023.vercel.app',
   },
 ]
 
